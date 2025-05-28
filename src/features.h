0ac08a7a6cde05ebbd93adff0120f112860d92a2
Agathe-BC <190834830+Agathe-BC@users.noreply.github.com>
2025-05-28 15:29:24 +0200

    mise en place 1

--- src/features.h
+++ src/features.h
@@ -3,8 +3,15 @@
 
 
 void helloWorld();
-void dimension (char *source_path);
-void first_pixel (char *source_path);
-void tenth_pixel (char *source_path);
+void dimension (char *source_path); /*25*/
+void first_pixel (char *source_path); /*24*/
+void tenth_pixel (char *source_path); /*23*/
+void second_line(char *source_path); /*22*/
+void print_pixel( char *filename, int x, int y ); /*21*/
+void max_pixel (char *filename); /*20*/
+void min_pixel (char *filename); /*19*/
+void max_component (char *filename); /*18*/
+void min_component (char *filename); /*17*/
+
 
 #endif
