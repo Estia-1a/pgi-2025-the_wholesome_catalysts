33ae526dbe7a02419179cde4e5a51604c7e15e8c
vaalleennttiinn <190834715+vaalleennttiinn@users.noreply.github.com>
2025-05-28 14:47:39 +0200

    tenth_pixel

--- src/main.c
+++ src/main.c
@@ -44,5 +44,9 @@ int main(int argc, char **argv) {
     /* first_pixel() function is defined in feature.h and implemented in feature.c */
     first_pixel( configuration.filenames[0] );
   }
+  if ( strncmp( configuration.command, "tenth_pixel", 12 ) == 0 ) {
+    /* tenth_pixel() function is defined in feature.h and implemented in feature.c */
+    tenth_pixel( configuration.filenames[0] );
+  }
   return 0;
 }
