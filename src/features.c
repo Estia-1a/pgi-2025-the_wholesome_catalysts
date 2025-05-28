bade0b3b5fe6f35a0966b85b0432c4fd15a083b6
vaalleennttiinn <190834715+vaalleennttiinn@users.noreply.github.com>
2025-05-28 16:37:17 +0200

    mise a jour

--- src/features.c
+++ src/features.c
@@ -10,109 +10,152 @@
  * When the feature is totally implemented, your commit message must contain "close #n".
  */
 
-void helloWorld() {
+void helloWorld()
+{
     printf("Hello World !");
 }
 
-void dimension (char *source_path){
+void dimension(char *source_path)
+{
 
-    unsigned char* data;
+    unsigned char *data;
 
-    int width =0;
+    int width = 0;
     int height = 0;
     int channel_count = 0;
-    
+
     int result = read_image_data(source_path, &data, &width, &height, &channel_count);
-    
-    if (result > 0){
+
+    if (result > 0)
+    {
 
         printf("dimension: %d, %d\n", width, height);
     }
-    return ;
-
+    return;
 }
 
-void first_pixel (char *source_path){
+void first_pixel(char *source_path)
+{
     int width, height, channel_count;
-    unsigned char* data;
+    unsigned char *data;
 
     read_image_data(source_path, &data, &width, &height, &channel_count);
 
     unsigned char R = data[0];
     unsigned char G = data[1];
     unsigned char B = data[2];
-    printf("first_pixel: %d, %d, %d",R,G,B);
+    printf("first_pixel: %d, %d, %d", R, G, B);
 }
 
-void tenth_pixel (char *source_path){
+void tenth_pixel(char *source_path)
+{
     int width, height, channel_count;
-    unsigned char* data;
+    unsigned char *data;
 
     read_image_data(source_path, &data, &width, &height, &channel_count);
 
-    int ligne_pixel = 9;                    /*pixel de la 10eme ligne */
-    int indice_pixel = ligne_pixel * 3;     /*Chaque pixel est composé de 3 bytes*/
-    
+    int ligne_pixel = 9;                /*pixel de la 10eme ligne */
+    int indice_pixel = ligne_pixel * 3; /*Chaque pixel est composé de 3 bytes*/
+
     unsigned char R = data[indice_pixel];
     unsigned char G = data[indice_pixel + 1];
     unsigned char B = data[indice_pixel + 2];
 
-    printf("tenth_pixel: %d, %d, %d",R,G,B);
-
+    printf("tenth_pixel: %d, %d, %d", R, G, B);
 }
 
 /* #22 */
 
-void second_line(char *source_path){            
+void second_line(char *source_path)
+{
+    int width, height, channel_count;
+    unsigned char *data;
 
+    read_image_data(source_path, &data, &width, &height, &channel_count);
 
+    int ligne = 1;                                    /* seconde ligne (index 1) */
+    int colonne = 0;                                  /* premier pixel de la ligne */
+    int indice_pixel = (ligne * width + colonne) * 3; /* Calcul correct de l'indice */
 
+    unsigned char R = data[indice_pixel];
+    unsigned char G = data[indice_pixel + 1];
+    unsigned char B = data[indice_pixel + 2];
 
-
-
-
+    printf("tenth_pixel: %d, %d, %d", R, G, B);
 }
 
 /* #21 */
 
-void print_pixel( char *filename, int x, int y ){       
-
-
-
-
-
-
-
+void print_pixel(char *filename, int x, int y)
+{
 }
 
 /* #20 */
-void max_pixel (char *filename){
-
-
-
-
-
+void max_pixel(char *filename)
+{
 }
 
 /* 19 */
 
-void min_pixel (char *filename){
-
-
-
-
+void min_pixel(char *filename)
+{
 }
 
 /* 18 */
 
-void max_component(char *filename, char component){
-
-
-
+void max_component(char *filename)
+{
 }
 
-void min_component(char *filename, char component){
+void min_component(char *filename)
+{
+}
 
-
-    
+void stat_report(char *filename)
+{
+}
+void color_red(char *filename)
+{
+}
+void color_green(char *filename)
+{
+}
+void color_blue(char *filename)
+{
+}
+void color_gray(char *filename)
+{
+}
+void invert(char *filename)
+{
+}
+void color_gray_luminance(char *filename)
+{
+}
+void rotate_cw(char *filename)
+{
+}
+void rotate_acw(char *filename)
+{
+}
+void mirror_horizontal(char *filename)
+{
+}
+void mirror_vertical(char *filename)
+{
+}
+void mirror_total(char *filename)
+{
+}
+void scale_crop(char *filename)
+{
+}
+void scale_nearest(char *filename)
+{
+}
+void scale_bilinear(char *filename)
+{
+}
+void color_desaturate(char *filename)
+{
 }
