0e7bc17afdd3bf27f9e10830a05bf05cf61ddfd2
Agathe-BC <190834830+Agathe-BC@users.noreply.github.com>
2025-06-13 17:29:19 +0200

    correction color_gray

--- src/features.c
+++ src/features.c
@@ -417,6 +417,9 @@ void color_blue(char *source_path)
     
     write_image_data("image_out.bmp", donnee, largeur, hauteur);
 }
+
+/*#12*/
+
 void color_gray(char *source_path) {    
     int width, height, nb_cannaux;    
     unsigned char *data;    
@@ -431,15 +434,27 @@ void color_gray(char *source_path) {
         data[pixel_index + 1] = gray; // G = gray        
         data[pixel_index + 2] = gray; // B = gray  
     }
-      
  
     write_image_data("image_out.bmp", data, width, height);
 }
 
 /*#11*/
 
-void invert(char *filename)
+void invert(char *source_path)
 {
+    int width, height, channel_count;
+    unsigned char *data;
+    read_image_data(source_path, &data, &width, &height, &channel_count);
+    // Parcourir tous les pixels et inverser les couleurs
+    for (int i = 0; i < width * height; i++) {
+        int pixel_index = i * 3;
+        // Inverser chaque composante : nouvelle_valeur = 255 - ancienne_valeur
+        data[pixel_index] = 255 - data[pixel_index];         // R inversé
+        data[pixel_index + 1] = 255 - data[pixel_index + 1]; // G inversé
+        data[pixel_index + 2] = 255 - data[pixel_index + 2]; // B inversé
+    }
+    // Sauvegarder l'image modifiée
+    write_image_data("image_out.bmp", data, width, height);
 }
 
 /*#10*/
